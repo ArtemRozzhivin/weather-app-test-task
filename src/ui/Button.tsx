7eae7353dffb8722f2cc738210a5f8ef981f1257
ArtemRozzhivin <artiomrozzhyvin@gmail.com>
2024-08-15 13:58:43 +0300

    create ui components

---
+++ src/ui/Button.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import MuiButton from '@mui/material/Button';
+
+interface ButtonInterface {
+  id?: string;
+  onClick?: () => void;
+  className?: string;
+  children: any;
+  color?: 'primary' | 'inherit' | 'secondary' | 'success' | 'error' | 'info' | 'warning';
+  variant?: 'text' | 'outlined' | 'contained';
+}
+
+const Button: React.FC<ButtonInterface> = ({
+  id,
+  onClick,
+  children,
+  color = 'primary',
+  variant = 'outlined',
+}) => {
+  return (
+    <MuiButton id={id} fullWidth color={color} variant={variant} onClick={onClick}>
+      {children}
+    </MuiButton>
+  );
+};
+
+export default Button;
---
+++ src/ui/Input.tsx
@@ -0,0 +1,25 @@
+import TextField from '@mui/material/TextField';
+import { useTheme } from '@mui/material/styles';
+import React from 'react';
+
+interface InputInterface {
+  value?: string;
+  onChange: (value: string) => void;
+  className?: string;
+  placeholder?: string | null;
+}
+
+const Input: React.FC<InputInterface> = ({ value, onChange, className, placeholder }) => {
+  return (
+    <TextField
+      fullWidth
+      color='primary'
+      label={placeholder}
+      variant='outlined'
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+    />
+  );
+};
+
+export default Input;
